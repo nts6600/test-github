5c3c780a4eddab8b5483aa7d66397987aa5b98be
Se-Hun <tpgns0539@naver.com>
2018-11-23 20:13:03 +0900

    택민이꺼 고쳐줌

--- Calculator.java
+++ Calculator.java
@@ -6,5 +6,11 @@ public class Calculator(){
   public int minus(int a, int b){
     return a - b;
   }
+  public int multiple(int a, int  b) {
+    return a * b;
+  }
+  public int divide(int a, int b) {
+    return a / b;
+  }
 
 }
